41fe488061f3164c0c1b2b66a12ed7d7528a8a86
pedalka <pedalka@gmail.com>
2015-12-08 16:07:40 +0200

    changing inheritance of MainActivity and inheritance of AppTheme (not required, but desirable)

--- app/src/main/java/com/example/boriskalim/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/boriskalim/tipcalculator/MainActivity.java
@@ -1,27 +1,25 @@
 package com.example.boriskalim.tipcalculator;
 
 
-    import android.app.Activity;
-    import android.content.Context;
-    import android.support.v7.app.ActionBarActivity;
-    import android.os.Bundle;
-    import android.view.LayoutInflater;
-    import android.view.Menu;
-    import android.view.MenuItem;
-    import android.view.View;
-    import android.view.ViewGroup;
-    import android.widget.BaseAdapter;
-    import android.widget.EditText;
-    import android.widget.ListAdapter;
-    import android.widget.ListView;
-    import android.widget.RadioButton;
-    import android.widget.TextView;
-    import android.widget.Toast;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
-    import java.util.ArrayList;
+import java.util.ArrayList;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     public ArrayList<String[]> namesAndPrices = new ArrayList<>();
     private ListView listView;
